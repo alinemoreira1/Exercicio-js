9b36e16bf5c12ca10cfb4b0216a1771444c7a162
Natália Reiko Sakamoto <nataliareikosakamoto@gmail.com>
2019-02-15 18:09:49 -0200

    Funcionalidade básica do jogogit config user.email nataliareikosakamoto@gmail.com

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 // const xo = document.querySelector('xo');
 // const suaVez = function () {
 //     a2.classList.toggle('x');
-    
+
 // }
 
 // a2.onclick = suaVez;
@@ -20,29 +20,56 @@
 // a2.innerHTML="O";
 // a1.innerHTML="x";
 
-let xo = document.querySelectorAll('.grid .xo'); 
+let quadrados = document.querySelectorAll('.grid .xo'); 
 let xCss = document.querySelector('.x'); 
 let oCss = document.querySelector('.o'); 
 let x = '<h4>X</h4>';
 let o = '<h4>O</h4>';
-let XorO = false;  
+let jogador2 = false;  
 
-let vitorias = []
+let jogadasP1 = [];
+let jogadasP2 = [];
 
-for(i = 0; i < xo.length; i++){ 
-    let quadradinho = xo[i];
-    quadradinho.onclick = function(){
-        if(XorO == false){  
-            if(quadradinho.innerHTML == ''){ 
-                quadradinho.innerHTML = x; 
-                XorO = true; 
-            }
-        } 
-        else if(XorO == true){ 
-            if(quadradinho.innerHTML == ''){ 
-                quadradinho.innerHTML = o; 
-                XorO = false; 
-            }
-        }
+const checarVitoria = function () {
+    // [0, 1, 2].
+    // [3, 4, 5].
+    // [6, 7, 8].
+    // [0, 3, 6].
+    // [1, 4, 7].
+    // [2, 5, 8].
+    // [0, 4, 8].
+    // [2, 4, 6].
+    if (quadrados[0].innerHTML === quadrados[1].innerHTML && quadrados[1].innerHTML === quadrados[2].innerHTML && quadrados[0].innerHTML !== "" || quadrados[3].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML === quadrados[5].innerHTML && quadrados[3].innerHTML !== ""
+    || quadrados[6].innerHTML === quadrados[7].innerHTML && quadrados[7].innerHTML === quadrados[8].innerHTML && quadrados[6].innerHTML !== ""
+    || quadrados[0].innerHTML === quadrados[3].innerHTML && quadrados[3].innerHTML === quadrados[6].innerHTML && quadrados[0].innerHTML !== ""
+    || quadrados[1].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML === quadrados[7].innerHTML && quadrados[1].innerHTML !== ""
+    || quadrados[2].innerHTML === quadrados[5].innerHTML && quadrados[5].innerHTML === quadrados[8].innerHTML && quadrados[2].innerHTML !== ""
+    || quadrados[0].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML === quadrados[8].innerHTML && quadrados[0].innerHTML !== ""
+    || quadrados[2].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML === quadrados[6].innerHTML && quadrados[2].innerHTML !== ""){
+        alert(`game over`);
     }
 }
+
+const processarClique = function(quadrado, indice){
+    if(jogador2 == false){  
+        quadrado.innerHTML = x;
+        jogadasP1.push(indice);
+        jogador2 = true; 
+    } 
+    else if(jogador2 == true){ 
+        quadrado.innerHTML = o;
+        jogadasP2.push(indice); 
+        jogador2 = false; 
+    }
+    
+    quadrado.onclick = null;
+    checarVitoria();
+}
+
+for(let i = 0; i < quadrados.length; i++){ 
+    let quadradinho = quadrados[i];
+    quadradinho.onclick = function () {
+        processarClique(quadradinho, i);
+    }
+}
+
